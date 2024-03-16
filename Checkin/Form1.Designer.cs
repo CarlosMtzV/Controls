b417284c950272b1f876d087dead87e5f0eb5aeb
CarlosMtzV <159225079+CarlosMtzV@users.noreply.github.com>
2024-03-15 20:19:20 -0600

    fff

---
+++ Checkin/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Checkin
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ListViewItem listViewItem1 = new ListViewItem("ola pepe");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            checkBox1 = new CheckBox();
+            checkedListBox1 = new CheckedListBox();
+            comboBox1 = new ComboBox();
+            dateTimePicker1 = new DateTimePicker();
+            linkLabel1 = new LinkLabel();
+            listBox1 = new ListBox();
+            listView1 = new ListView();
+            maskedTextBox1 = new MaskedTextBox();
+            numericUpDown1 = new NumericUpDown();
+            progressBar1 = new ProgressBar();
+            radioButton1 = new RadioButton();
+            textBox1 = new TextBox();
+            radioButton2 = new RadioButton();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.BackColor = SystemColors.Control;
+            checkBox1.Font = new Font("Microsoft Sans Serif", 8.999999F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            checkBox1.Location = new Point(12, 12);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(139, 19);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Eres Tecano? UwU?";
+            checkBox1.UseVisualStyleBackColor = false;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Items.AddRange(new object[] { "uwu", "#WINING" });
+            checkedListBox1.Location = new Point(181, 12);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(120, 94);
+            checkedListBox1.TabIndex = 1;
+            checkedListBox1.Tag = "";
+            checkedListBox1.SelectedIndexChanged += checkedListBox1_SelectedIndexChanged;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Hola", "Adios ", "Que onda", "Chispas", "Sopas" });
+            comboBox1.Location = new Point(344, 8);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(498, 12);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(230, 23);
+            dateTimePicker1.TabIndex = 3;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(716, 38);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(72, 15);
+            linkLabel1.TabIndex = 4;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Yutube.com";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Items.AddRange(new object[] { "Muuito", "Sazon", "GG", "ez", "Nub" });
+            listBox1.Location = new Point(12, 135);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 94);
+            listBox1.TabIndex = 5;
+            // 
+            // listView1
+            // 
+            listView1.Items.AddRange(new ListViewItem[] { listViewItem1 });
+            listView1.Location = new Point(195, 136);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(121, 97);
+            listView1.TabIndex = 6;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(363, 136);
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(100, 23);
+            maskedTextBox1.TabIndex = 7;
+            maskedTextBox1.Text = "ola uwu";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(539, 128);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 8;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(559, 185);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(100, 23);
+            progressBar1.TabIndex = 9;
+            progressBar1.Click += progressBar1_Click;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.BackColor = SystemColors.Control;
+            radioButton1.Location = new Point(371, 189);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(41, 19);
+            radioButton1.TabIndex = 10;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "ola";
+            radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(688, 136);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 12;
+            textBox1.Text = "HU yi bi fan";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.BackColor = SystemColors.Control;
+            radioButton2.Location = new Point(371, 214);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(41, 19);
+            radioButton2.TabIndex = 15;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "bai";
+            radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(636, 263);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(152, 126);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 16;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(363, 277);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 46);
+            button1.TabIndex = 17;
+            button1.Text = "bienvenidos";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkSeaGreen;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(radioButton2);
+            Controls.Add(textBox1);
+            Controls.Add(radioButton1);
+            Controls.Add(progressBar1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(listView1);
+            Controls.Add(listBox1);
+            Controls.Add(linkLabel1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(comboBox1);
+            Controls.Add(checkedListBox1);
+            Controls.Add(checkBox1);
+            Name = "Form1";
+            Text = "                  ";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private CheckedListBox checkedListBox1;
+        private ComboBox comboBox1;
+        private DateTimePicker dateTimePicker1;
+        private LinkLabel linkLabel1;
+        private ListBox listBox1;
+        private ListView listView1;
+        private MaskedTextBox maskedTextBox1;
+        private NumericUpDown numericUpDown1;
+        private ProgressBar progressBar1;
+        private RadioButton radioButton1;
+        private TextBox textBox1;
+        private RadioButton radioButton2;
+        private PictureBox pictureBox1;
+        private Button button1;
+    }
+}
---
+++ Checkin/Form1.cs
@@ -0,0 +1,45 @@
+namespace Checkin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("HOla te dice el TEC");
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
